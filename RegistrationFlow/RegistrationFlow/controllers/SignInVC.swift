f6268c57ad3b0aecdecf498fd8999e590114e205
sergey-stack <Kasach.serzh@mail.ru>
2022-08-01 15:00:59 +0300

    SignIn commit 1

---
+++ RegistrationFlow/RegistrationFlow/controllers/SignInVC.swift
@@ -0,0 +1,33 @@
+//
+//  SignInVC.swift
+//  RegistrationFlow
+//
+//  Created by сергей on 1.08.22.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    @IBOutlet weak var errorPass: UILabel!
+    @IBOutlet weak var passTF: UITextField!
+    @IBOutlet weak var errorEmail: UILabel!
+    @IBOutlet weak var emailTF: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
